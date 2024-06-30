e2f9a2508fe8a42af9c7c358074c254338951ce9
Thivagar1494 <thivagarazurecloud2595@gmail.com>
2024-07-01 03:15:43 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "demo-glb-debt-crm-rg"
+  default     = "glb-debt-crm-rg"
 }
 
 variable "location" {
