94d78f344bafd1f6e12b67affb16810cf8501373
Thivagar1494 <thivagarazurecloud2595@gmail.com>
2024-07-01 02:12:28 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.110.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {
-    
-  }
+  features {}
 }
 
 module "resource_group" {
